f5e4219e7a854ed0a72a0a91ec85a87a355232de
Muhammad Baharuddin Yusuf <baharudinplh2018@gmail.com>
2024-12-16 00:46:10 +0800

    update image morphology

--- app.py
+++ app.py
@@ -7,32 +7,32 @@ import os
 img_gray = cv2.imread("images/original.jpg", cv2.IMREAD_GRAYSCALE)
 cv2.imwrite('images/grayscale.jpg', img_gray)
 
+# Thresholding
+img_gray = cv2.imread('images/grayscale.jpg', cv2.IMREAD_GRAYSCALE)
+threshold_value = 127
+ret, thresholded_img = cv2.threshold(img_gray, threshold_value, 255, cv2.THRESH_BINARY)
+cv2.imwrite('images/thresholded_image.jpg', thresholded_img)
+
 # Deteksi tepi
-edges = cv2.Canny(img_gray, 50, 150, apertureSize=3)
+edges = cv2.Canny(thresholded_img, 50, 150, apertureSize=3)
 cv2.imwrite("images/edges.jpg", edges)
 
-# Operasi dilasi
-kernel = [
-    [0,0,0],
-    [1,1,1],
-    [0,1,0]
-  ]
-kernel = np.array(kernel, dtype=np.uint8)
-
-dilated_edges = cv2.dilate(edges, kernel, iterations=1)
-cv2.imwrite("images/dilation.jpg", dilated_edges)
+# Operasi closing
+kernel = np.ones((12,12),np.uint8)
+closing = cv2.morphologyEx(edges, cv2.MORPH_CLOSE, kernel)
+cv2.imwrite('images/closing.jpg', closing)
 
 # Kompresi
-dilated_image = Image.fromarray(dilated_edges)
-compressed_image = dilated_image.resize((dilated_image.width // 2, dilated_image.height // 2))
-compressed_image.save("images/compressed_dilated_edges.jpg")
+closing_image = Image.fromarray(closing)
+compressed_image = closing_image.resize((closing_image.width // 2, closing_image.height // 2))
+compressed_image.save("images/compressed_image.jpg")
 
-original_size = os.path.getsize('images/dilation.jpg')
-compressed_size = os.path.getsize('images/compressed_dilated_edges.jpg')
+original_size = os.path.getsize('images/closing.jpg')
+compressed_size = os.path.getsize('images/compressed_image.jpg')
 
 compression_ratio_kb = (original_size / 1024) / (compressed_size / 1024)
 
-print(f"Dilated image size: {original_size/1024:.2f} KB")
+print(f"Closing image size: {original_size/1024:.2f} KB")
 print(f"Compressed image size: {compressed_size/1024:.2f} KB")
 print(f"Compression ratio: {compression_ratio_kb:.2f}")
 
@@ -44,11 +44,14 @@ print(f"Original image size: {original_size/1024:.2f} KB")
 gray_size = os.path.getsize('images/grayscale.jpg')
 print(f"Grayscale image size: {gray_size/1024:.2f} KB")
 
+gray_size = os.path.getsize('images/thresholded_image.jpg')
+print(f"Thresholded image size: {gray_size/1024:.2f} KB")
+
 edges_size = os.path.getsize('images/edges.jpg')
 print(f"Edges image size: {edges_size/1024:.2f} KB")
 
-dilated_size = os.path.getsize('images/dilation.jpg')
-print(f"Dilated image size: {dilated_size/1024:.2f} KB")
+dilated_size = os.path.getsize('images/closing.jpg')
+print(f"Closing image size: {dilated_size/1024:.2f} KB")
 
-compressed_size = os.path.getsize('images/compressed_dilated_edges.jpg')
+compressed_size = os.path.getsize('images/compressed_image.jpg')
 print(f"Compressed image size: {compressed_size/1024:.2f} KB")
